8147c8bb4a84834b99a2fae96cae9fa18f567ea8
doh17003@byui.edu <66549514+DevPrestigious@users.noreply.github.com>
2022-11-15 02:38:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.splatty',
+  appName: 'splatty',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
